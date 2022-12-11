c1b8f6cb94f0064ea52ab9c60d25124ca5552598
NicholasFeltrin <me@nicholasfeltrin.com>
2022-12-11 02:04:29 +0000

    dwmblocks and macros

--- config.def.h
+++ config.def.h
@@ -3,14 +3,14 @@
 #define TERMINAL "st"
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static const int swallowfloating    = 1; 	/*1 means swallow floating windows by default*/
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -80,9 +80,13 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+#define STATUSBAR "dwmblocks"
+
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { TERMINAL, NULL };
+
+#include <X11/XF86keysym.h>
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -99,27 +103,27 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                     	6)
 	TAGKEYS(                        XK_8,                     	7)
 	TAGKEYS(                        XK_9,                     	8)
-	{ MODKEY,			XK_0,		view,		{.ui = ~0 } },
-	{ MODKEY|ShiftMask,		XK_0,		tag,		{.ui = ~0 } },
+	{ MODKEY,			XK_0,		togglesticky,	{.ui = ~0 } },
+	{ MODKEY|ShiftMask,		XK_0,		view,		{.ui = ~0 } },
 	{ MODKEY,                       XK_j,     	focusstack,    	{.i = +1 } },
 	{ MODKEY,                       XK_k,     	focusstack,    	{.i = -1 } },
+	{ MODKEY,			XK_space,	zoom,		{0} },
 	{ MODKEY,			XK_q,	  	killclient,	{0} },
 	{ MODKEY|ControlMask|ShiftMask, XK_Escape,     	quit,          	{0} },
 
 	{ MODKEY,			XK_t,		setlayout,	{.v = &layouts[0]} }, /* tile */
-	{ MODKEY|ShiftMask,		XK_t,		setlayout,	{.v = &layouts[1]} }, /* bstack */
-	{ MODKEY,			XK_y,		setlayout,	{.v = &layouts[2]} }, /* spiral */
-	{ MODKEY|ShiftMask,		XK_y,		setlayout,	{.v = &layouts[3]} }, /* dwindle */
+	/* { MODKEY|ShiftMask,		XK_t,		setlayout,	{.v = &layouts[1]} },*/ /* bstack */
+	/* { MODKEY,			XK_y,		setlayout,	{.v = &layouts[2]} },*/ /* spiral */
+	/* { MODKEY|ShiftMask,		XK_y,		setlayout,	{.v = &layouts[3]} },*/ /* dwindle */
 	{ MODKEY,			XK_u,		setlayout,	{.v = &layouts[4]} }, /* deck */
-	{ MODKEY|ShiftMask,		XK_u,		setlayout,	{.v = &layouts[5]} }, /* monocle */
-	{ MODKEY,			XK_i,		setlayout,	{.v = &layouts[6]} }, /* centeredmaster */
-	{ MODKEY|ShiftMask,		XK_i,		setlayout,	{.v = &layouts[7]} }, /* centeredfloatingmaster */
-	{ MODKEY|ShiftMask,		XK_f,		setlayout,	{.v = &layouts[8]} },
+	{ MODKEY,			XK_y,		setlayout,	{.v = &layouts[5]} }, /* monocle */
+	/*{ MODKEY,			XK_i,		setlayout,	{.v = &layouts[6]} },*/ /* centeredmaster */
+	{ MODKEY,			XK_i,		setlayout,	{.v = &layouts[7]} }, /* centeredfloatingmaster */
+	/* { MODKEY|ShiftMask,		XK_f,		setlayout,	{.v = &layouts[8]} },*/
 
-	{ MODKEY,			XK_s,		togglesticky,	{0} },
+	{ MODKEY,			XK_s,		togglefloating,	{0} },
 	{ MODKEY,			XK_f,		togglefullscr,	{0} },
-	{ MODKEY,			XK_space,	zoom,		{0} },
-	{ MODKEY|ShiftMask,		XK_space,	togglefloating,	{0} },
+	/* { MODKEY|ShiftMask,		XK_space,	togglefloating,	{0} }, */
 
 	{ MODKEY,			XK_o,		incnmaster,     {.i = +1 } },
 	{ MODKEY|ShiftMask,		XK_o,		incnmaster,     {.i = -1 } },
@@ -135,18 +139,20 @@ static const Key keys[] = {
 	/**/
 	/*Media*/
 	/**/
-	{ 0, XF86XK_AudioMute,		spawn,		SHCMD("amixer -c 2 set 'Master' toggle") },
-	{ 0, XF86XK_AudioRaiseVolume,	spawn,		SHCMD("amixer -c 2 set 'Master' 5%+") },
-	{ 0, XF86XK_AudioLowerVolume,	spawn,		SHCMD("amixer -c 2 set 'Master' 5%-") },
+	{ 0, XF86XK_AudioMute,		spawn,		SHCMD("amixer -c 2 set 'Master' toggle; kill -44 $(pidof dwmblocks)") },
+	{ 0, XF86XK_AudioRaiseVolume,	spawn,		SHCMD("amixer -c 2 set 'Master' 5%+; kill -44 $(pidof dwmblocks)") },
+	{ 0, XF86XK_AudioLowerVolume,	spawn,		SHCMD("amixer -c 2 set 'Master' 5%-; kill -44 $(pidof dwmblocks)") },
 
-	{ 0, XF86AudioPrev,		spawn,		SHCMD("mpc prev") },
+	{ 0, XF86XK_AudioPrev,		spawn,		SHCMD("mpc prev") },
 	{ MODKEY, XK_F5,		spawn,		SHCMD("mpc prev") },
-	{ 0, XF86AudioNext,		spawn,		SHCMD("mpc next") },
+	{ 0, XF86XK_AudioNext,		spawn,		SHCMD("mpc next") },
 	{ MODKEY, XK_F6,		spawn,		SHCMD("mpc next") },
-	{ 0, XF86AudioPlay,		spawn,		SHCMD("mpc toggle") },
+	{ 0, XF86XK_AudioPlay,		spawn,		SHCMD("mpc toggle") },
 	{ MODKEY, XK_F7,		spawn,		SHCMD("mpc toggle") },
-	{ 0, XF86AudioStop,		spawn,		SHCMD("mpc repeat") },
+	{ 0, XF86XK_AudioStop,		spawn,		SHCMD("mpc repeat") },
 	{ MODKEY, XK_F8,		spawn,		SHCMD("mpc repeat") },
+	{ MODKEY,			XK_comma,	spawn,		SHCMD("mpc prev") },
+	{ MODKEY,			XK_period,	spawn,		SHCMD("mpc next") },
 	{ MODKEY,			XK_p,			spawn,		SHCMD("mpc toggle") },
 	{ MODKEY,			XK_Left,		spawn,		SHCMD("mpc seek -10") },
 	{ MODKEY|ShiftMask,		XK_Left,		spawn,		SHCMD("mpc seek -60") },
@@ -164,7 +170,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_w,		spawn,		SHCMD("firefox") },
 	{ MODKEY,			XK_e,		spawn,		SHCMD("$EMAIL") },
 	{ MODKEY,			XK_v,		spawn,		SHCMD("virt-manager") },
-	{ MODKEY|ShiftMask,		XK_r,		spawn,		SHCMD(TERMINAL " -e htop") },
+	{ MODKEY,			XK_r,		spawn,		SHCMD(TERMINAL " -e htop") },
 	{ MODKEY,			XK_m,		spawn,		SHCMD(TERMINAL " -e ncmpcpp") },
 	{ MODKEY|ShiftMask,		XK_m,		spawn,		SHCMD(TERMINAL " -e alsamixer") },
 };
@@ -176,7 +182,9 @@ static const Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button1,        sigstatusbar,   {.i = 1} },
+	{ ClkStatusText,        0,              Button2,        sigstatusbar,   {.i = 2} },
+	{ ClkStatusText,        0,              Button3,        sigstatusbar,   {.i = 3} },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const unsigned int gappiv    = 10;       /* vert inner gap between window
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static const int swallowfloating    = 1; 	/*1 means swallow floating windows by default*/
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -80,6 +80,8 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+#define STATUSBAR "dwmblocks"
+
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { TERMINAL, NULL };
@@ -137,9 +139,9 @@ static const Key keys[] = {
 	/**/
 	/*Media*/
 	/**/
-	{ 0, XF86XK_AudioMute,		spawn,		SHCMD("amixer -c 2 set 'Master' toggle") },
-	{ 0, XF86XK_AudioRaiseVolume,	spawn,		SHCMD("amixer -c 2 set 'Master' 5%+") },
-	{ 0, XF86XK_AudioLowerVolume,	spawn,		SHCMD("amixer -c 2 set 'Master' 5%-") },
+	{ 0, XF86XK_AudioMute,		spawn,		SHCMD("amixer -c 2 set 'Master' toggle; kill -44 $(pidof dwmblocks)") },
+	{ 0, XF86XK_AudioRaiseVolume,	spawn,		SHCMD("amixer -c 2 set 'Master' 5%+; kill -44 $(pidof dwmblocks)") },
+	{ 0, XF86XK_AudioLowerVolume,	spawn,		SHCMD("amixer -c 2 set 'Master' 5%-; kill -44 $(pidof dwmblocks)") },
 
 	{ 0, XF86XK_AudioPrev,		spawn,		SHCMD("mpc prev") },
 	{ MODKEY, XK_F5,		spawn,		SHCMD("mpc prev") },
@@ -149,6 +151,8 @@ static const Key keys[] = {
 	{ MODKEY, XK_F7,		spawn,		SHCMD("mpc toggle") },
 	{ 0, XF86XK_AudioStop,		spawn,		SHCMD("mpc repeat") },
 	{ MODKEY, XK_F8,		spawn,		SHCMD("mpc repeat") },
+	{ MODKEY,			XK_comma,	spawn,		SHCMD("mpc prev") },
+	{ MODKEY,			XK_period,	spawn,		SHCMD("mpc next") },
 	{ MODKEY,			XK_p,			spawn,		SHCMD("mpc toggle") },
 	{ MODKEY,			XK_Left,		spawn,		SHCMD("mpc seek -10") },
 	{ MODKEY|ShiftMask,		XK_Left,		spawn,		SHCMD("mpc seek -60") },
@@ -178,7 +182,9 @@ static const Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button1,        sigstatusbar,   {.i = 1} },
+	{ ClkStatusText,        0,              Button2,        sigstatusbar,   {.i = 2} },
+	{ ClkStatusText,        0,              Button3,        sigstatusbar,   {.i = 3} },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
