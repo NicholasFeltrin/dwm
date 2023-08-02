ce6d890131ef3700847b05576b32091047980157
NicholasFeltrin <me@nicholasfeltrin.com>
2023-08-02 08:21:27 +0800

    updated configs

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const unsigned int gappiv    = 10;       /* vert inner gap between window
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static const int swallowfloating    = 1; 	/*1 means swallow floating windows by default*/
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static char font[]            = "monospace:size=10";
@@ -179,7 +179,7 @@ static const Key keys[] = {
 	/**/
 	{ MODKEY,			XK_Return,	spawn,		{.v = termcmd } },
 	{ MODKEY,			XK_w,		spawn,		SHCMD("$BROWSER") },
-	{ MODKEY|ShiftMask,		XK_w,		spawn,		SHCMD("firefox") },
+	{ MODKEY|ShiftMask,		XK_w,		spawn,		SHCMD("tor-browser") },
 	{ MODKEY,			XK_e,		spawn,		SHCMD("$EMAIL") },
 	{ MODKEY,			XK_v,		spawn,		SHCMD("virt-manager") },
 	{ MODKEY,			XK_r,		spawn,		SHCMD(TERMINAL " -e htop") },
